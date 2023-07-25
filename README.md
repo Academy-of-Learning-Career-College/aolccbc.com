e70281bd8d33210396bb58c654b033d34bc416cd
mike <mike@aolccbc.com>
2023-07-25 12:12:53 -0700

    update owner

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # aolccbc.com
 ![Website](https://img.shields.io/website?down_color=red&down_message=offline&label=aolccbc.com&up_color=green&up_message=online&url=https%3A%2F%2Faolccbc.com)
 
-![Libraries.io dependency status for GitHub repo](https://img.shields.io/librariesio/github/fireball8931/aolccbc.com)
+![Libraries.io dependency status for GitHub repo](https://img.shields.io/librariesio/github/Academy-of-Learning-Career-College/aolccbc.com)
 
 This is the source code for aolccbc.com
 
@@ -20,7 +20,7 @@ This project has been converted to use parcel to build the static pages.
 
 To develop/build, you need node.js
 
-- Clone the project by running `git clone https://github.com/fireball8931/aolccbc.com`
+- Clone the project by running `git clone https://github.com/Academy-of-Learning-Career-College/aolccbc.com`
 - CD into aolccbc.com
 - Run ``` npm i ```
 - Run ``` npm run dev ``` to develop
--- static/staff/parsejson.js
+++ static/staff/parsejson.js
@@ -1,13 +1,13 @@
 console.log(window.location.search);
 let staff = window.location.search.replace("?","")
 let cellphone = "";
-fetch("https://github.com/fireball8931/aolccbc.com/raw/main/www/staff/" + staff + ".json")
+fetch("https://github.com/Academy-of-Learning-Career-College/aolccbc.com/raw/main/www/staff/" + staff + ".json")
         .then(function(response) {
             return response.json()
         })
         .then(function(data) {
 //            console.log(data)
-            document.getElementById('avatar').innerHTML = "<img src='https://github.com/fireball8931/aolccbc.com/raw/main/www/staff/avatars/" + staff + ".png?as=webp' alt='Profile Picture' class='avatar'>";
+            document.getElementById('avatar').innerHTML = "<img src='https://github.com/Academy-of-Learning-Career-College/aolccbc.com/raw/main/www/staff/avatars/" + staff + ".png?as=webp' alt='Profile Picture' class='avatar'>";
             document.getElementById('name').innerHTML = data.name;
             document.getElementById('title').innerHTML = data.title;
             document.getElementById('campus').innerHTML = `Academy of Learning Career College | ${data.campus}`;
@@ -15,28 +15,28 @@ fetch("https://github.com/fireball8931/aolccbc.com/raw/main/www/staff/" + staff 
             document.getElementById('mailto').href = `mailto:${data.email}`;
             //document.getElementById('phone').innerHTML = `Academy of Learning Career College ${data.campus}`;
             if(data.cell !== undefined){
-               
+
                 cellphone = ` | <span class='title'>Cell: </span>${data.cell} `;
             }
-            
-            
-            
+
+
+
             let thecampus = data.campus;
-            fetch("https://github.com/fireball8931/aolccbc.com/raw/main/www/staff/campusinfo.json")
+            fetch("https://github.com/Academy-of-Learning-Career-College/aolccbc.com/raw/main/www/staff/campusinfo.json")
             .then(function(response) {
                 return response.json()
-            }) 
+            })
             .then(function(data){
                 data.campus.forEach(campus => {
                     if(campus.name == thecampus ){
                         document.getElementById('phonefax').innerHTML = "<span class='title'>Phone: </span>" + campus.phone + " | <span class='title'>Fax: </span>" + campus.fax;
                         document.getElementById('address').innerHTML = "<span class='title'>Address: </span>" + campus.address;
                         document.getElementById('tf').innerHTML = "<span class='title'>Toll Free: </span>" + campus.tf + cellphone;
-                        return; 
+                        return;
                     }
                 });
 
             });
-            
+
               })
 
